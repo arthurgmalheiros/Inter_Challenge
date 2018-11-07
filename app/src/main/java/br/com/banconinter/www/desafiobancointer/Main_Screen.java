83f082c7b71032ec8d8cee64d23f92682e8e47f7
Arthur Malheiros <arthurgoncalvesmalheiros@gmail.com>
2018-11-07 19:33:43 -0200

    botao acrescentado

--- app/src/main/java/br/com/banconinter/www/desafiobancointer/Main_Screen.java
+++ app/src/main/java/br/com/banconinter/www/desafiobancointer/Main_Screen.java
@@ -1,7 +1,11 @@
 package br.com.banconinter.www.desafiobancointer;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main_Screen extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class Main_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__screen);
+
+    final Button button = (Button) findViewById(R.id.button3);
+         button.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+            Intent myIntent = new Intent(this, .class);
+            startActivity(myIntent);
+        }
+    });
+        }
     }
-}
+
---
+++ app/src/main/java/br/com/banconinter/www/desafiobancointer/Second_Screen.java
@@ -0,0 +1,13 @@
+package br.com.banconinter.www.desafiobancointer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Second_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second__screen);
+    }
+}
