7af09153ef65c5743504988a5bb0ee38025ccb22
huron_hare <bertod777@gmail.com>
2018-11-07 20:45:09 -0200

    Added Qr Reader Part 2
    Now its working

--- app/src/main/java/br/com/banconinter/www/desafiobancointer/Second_Screen.java
+++ app/src/main/java/br/com/banconinter/www/desafiobancointer/Second_Screen.java
@@ -1,7 +1,13 @@
 package br.com.banconinter.www.desafiobancointer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 
 public class Second_Screen extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class Second_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second__screen);
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        integrator.setPrompt("Camera Scan");
+        integrator.setCameraId(0);
+        integrator.initiateScan();
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result != null){
+            if (result.getContents() !=  null){
+                alert(result.getContents());
+            }else{
+                alert("Scan cancelado");
+            }
+        }else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    private void alert(String msg){
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
     }
 }
