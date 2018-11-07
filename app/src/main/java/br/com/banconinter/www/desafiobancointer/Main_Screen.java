2c039f1ae0968e073d1264d08a770bb9d3bfda53
Arthur Malheiros <arthurgoncalvesmalheiros@gmail.com>
2018-11-07 21:00:29 -0200

    merda

--- app/src/main/java/br/com/banconinter/www/desafiobancointer/Main_Screen.java
+++ app/src/main/java/br/com/banconinter/www/desafiobancointer/Main_Screen.java
@@ -13,14 +13,6 @@ public class Main_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main__screen);
-
-    final Button button = (Button) findViewById(R.id.button3);
-         button.setOnClickListener(new View.OnClickListener() {
-        public void onClick(View v) {
-            Intent myIntent = new Intent(this, .class);
-            startActivity(myIntent);
-        }
-    });
         }
     }
 
