fcf0d202037554ae1ea1ddeba560e6613736e0a4
Huron <zer0games2@hotmail.com>
2018-11-07 14:48:05 -0200

    Init
    Initial Release

---
+++ app/src/main/java/br/com/banconinter/www/desafiobancointer/Main_Screen.java
@@ -0,0 +1,13 @@
+package br.com.banconinter.www.desafiobancointer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__screen);
+    }
+}
