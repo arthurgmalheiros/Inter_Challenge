5962989bf7642cbba9d5a7b379c94c0426ca7891
huron_hare <bertod777@gmail.com>
2018-11-08 15:26:43 -0200

    added sliders and some math

--- app/src/main/java/br/com/banconinter/www/desafiobancointer/Second_Screen.java
+++ app/src/main/java/br/com/banconinter/www/desafiobancointer/Second_Screen.java
@@ -53,7 +53,7 @@ public class Second_Screen extends AppCompatActivity {
             Intent itn = new Intent(Second_Screen.this, imv.getClass());
             Bundle b = new Bundle();
             b.putString("cep", parts[1]);
-            b.putString("valor", parts[2]);
+            b.putInt("valor", Integer.parseInt(parts[2]));
             b.putString("condominio", parts[3]);
             b.putString("iptu", parts[4]);
             b.putString("area", parts[5]);
--- app/src/main/java/br/com/banconinter/www/desafiobancointer/Sim_Financiamento.java
+++ app/src/main/java/br/com/banconinter/www/desafiobancointer/Sim_Financiamento.java
@@ -1,19 +1,80 @@
 package br.com.banconinter.www.desafiobancointer;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
 import android.widget.TextView;
-
+import java.text.DecimalFormat;
 public class Sim_Financiamento extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sim__financiamento);
         Intent i = getIntent();
-        TextView txt = (TextView)findViewById(R.id.Titulo);
 
+        final Integer valor = (Integer)i.getSerializableExtra("valor");
+
+        final SeekBar skparc = (SeekBar) findViewById(R.id.sk_Parcelas);
+        Double minCred = valor * 0.3;
+        final SeekBar skmax = (SeekBar) findViewById(R.id.sk_Cred);
+        skmax.setMin(minCred.intValue());
+        skmax.setMax(valor.intValue());
+
+        final TextView jk = (TextView)findViewById(R.id.txt_Cred);
+        final TextView pf = (TextView)findViewById(R.id.txt_ParcFinal);
+        final TextView tp = (TextView)findViewById(R.id.txt_Parc);
+        jk.setText(skmax.getProgress() + "/" + valor.toString());
+        final double ueu = ((valor.intValue() - skmax.getProgress()) /skparc.getProgress()) * 1.0075;
+        final DecimalFormat format = new DecimalFormat("##.00");
+        pf.setText("R$" + format.format(ueu) );
+        skparc.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                final double ueu = ((valor.intValue() - skmax.getProgress()) / 1 +skparc.getProgress())* 1.0075;
+                tp.setText(Integer.toString(skparc.getProgress()));
+                pf.setText("R$" + format.format(ueu));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        skmax.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                jk.setText(skmax.getProgress() + "/" + valor.toString());
+                final double ueu =((valor - skmax.getProgress()) +(skparc.getProgress())* 1.0075);
+
+                pf.setText("R$" + format.format(ueu) );
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+
+
+        TextView txt = (TextView)findViewById(R.id.Titulo);
         txt.setText((String)i.getSerializableExtra("nome"));
 
     }
